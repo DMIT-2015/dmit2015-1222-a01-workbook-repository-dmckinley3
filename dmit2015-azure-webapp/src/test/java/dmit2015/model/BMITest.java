e647b1bbbcbd7fac29f8c67b5e6ddf798aecfd3b
David McKinley <dmckinley3@nait.ca>
2023-01-19 13:52:04 -0700

    add code to bmi class

--- dmit2015-azure-webapp/src/test/java/dmit2015/model/BMITest.java
+++ dmit2015-azure-webapp/src/test/java/dmit2015/model/BMITest.java
@@ -1,21 +1,12 @@
 package dmit2015.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class BMITest {
 
-//    @ParameterizedTest(name = "weight = {0}, height = {1} ")
-//    @CsvSource({
-//            "100,66",
-//            "140,66",
-//            "175,66",
-//            "200.0,66",
-//    })
-//    void bmiCategory(int weight, int height) {
-//
-//        //arrange
-//        BMI person = new BMI();
-//        //act
-//        person.setHeight(height);
-//        person.setWeight(weight);
-//        //assert
-//    }
+    @Test
+    void bmiCategory() {
+    }
 }
