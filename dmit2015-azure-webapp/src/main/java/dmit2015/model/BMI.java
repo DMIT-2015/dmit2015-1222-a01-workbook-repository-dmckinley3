443ae596a22d6297b10338ea0d851187ef73c284
David McKinley <dmckinley3@nait.ca>
2023-01-19 12:54:06 -0700

    add ex1 code

---
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/BMI.java
@@ -0,0 +1,49 @@
+package dmit2015.model;
+
+public class BMI {
+    public int weight;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int height;
+
+    public BMI() {
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double bmi() {
+        return (703 * weight) / (height * height);
+    }
+
+    public String bmiCategory() {
+        String bmiCat;
+        if (this.bmi() <= 30) {
+            if (this.bmi() < 18.5) {
+                bmiCat = "underweight";
+            }
+            if (this.bmi() >= 18.5 && bmi() <= 24.9) {
+                bmiCat = "normal";
+            }
+            if (this.bmi() >= 25 && bmi() <= 29.9) {
+                bmiCat = "overweight";
+            }
+        } else {
+            bmiCat = "obese";
+        }
+        return bmiCategory();
+    }
+
+}
