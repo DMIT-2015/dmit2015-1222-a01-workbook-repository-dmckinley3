8a7d8fa18b641211a927d624d2d03255fe31d923
David McKinley <dmckinley3@nait.ca>
2023-02-02 09:50:22 -0700

    partial wed class

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/model/Circle.java
@@ -0,0 +1,106 @@
+package dmit2015.faces.model;
+
+/**
+ * This class model is a circle shape
+ *
+ * @author David
+ * @version 2023.01.20
+ **/
+public class Circle {
+    /**
+     * The Radius of this circle.
+     **/
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    // Checked-exception example
+//    public void setRadius(double radius) throws Exception {
+//        if (radius <= 0) {
+//            throw new Exception("Radius must be a positive non-zero number");
+//        }
+//        this.radius = radius;
+//    }
+
+    // Runtime exception example
+
+    /**
+     * Change the radius of this circle.
+     *
+     * @param radius the new radius of the circle.
+     */
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            throw new RuntimeException("Radius must be a positive non-zero number");
+        }
+        this.radius = radius;
+    }
+
+    public Circle() {
+//        radius = 1;
+        setRadius(1);
+    }
+
+    public Circle(double radius) {
+        //this.radius = radius;
+        setRadius(radius);
+    }
+
+    /**
+     * Compute and return the area of this circle
+     *
+     * @return area of this circle
+     **/
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    /**
+     * Compute and return the diameter of this circle
+     *
+     * @return diameter of this circle
+     **/
+    public double diameter() {
+        return 2 * radius;
+    }
+
+    /**
+     * Compute and return the circumference of this circle
+     *
+     * @return circumference of this circle
+     **/
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        // Create a Circle with a radius of 1
+        Circle circle1 = new Circle();
+        // The radius of circle1 should be 1,
+        // area should be 3.14159
+        // perimeter should be 6.28
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.diameter());
+
+        // Change the radius of circle1 to 5
+        circle1.setRadius(5);
+        // The radius of circle1 should be 5,
+        // area should be 78.53982
+        // perimeter should be 31.41593
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.diameter());
+
+        // Change the radius of circle1 to -25
+        // It should throw an exception
+        try {
+            circle1.setRadius(-25);
+            System.out.println("A exception should have been thrown");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/model/Rectangle.java
@@ -0,0 +1,49 @@
+package dmit2015.faces.model;
+
+import lombok.Data;
+
+/**
+ * This class models a rectangle shape
+ *
+ * @author David
+ * @version 2023.01.20
+ **/
+@Data
+public class Rectangle {
+    /**
+     * The length of this Rectangle
+     **/
+    private double length;
+    /**
+     * The width of this Rectangle
+     **/
+    private double width;
+
+    /**
+     * Compute and return the area of this rectangle
+     *
+     * @return area of this circle
+     **/
+    public double area() {
+        return width * length;
+    }
+
+    /**
+     * Compute and return the perimeter of this rectangle
+     *
+     * @return perimeter of this circle
+     **/
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+
+    /**
+     * Compute and return the diagonal of this rectangle
+     *
+     * @return diagonal of this circle
+     **/
+    public double diagonal() {
+        return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+    }
+
+}
