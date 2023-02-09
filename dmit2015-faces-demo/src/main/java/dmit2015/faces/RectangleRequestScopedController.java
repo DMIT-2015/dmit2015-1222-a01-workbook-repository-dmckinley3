5046464cc5be78ba6e9db6428bb6844d89c67c48
David McKinley <dmckinley3@nait.ca>
2023-02-09 08:07:33 -0700

    add wed class

--- dmit2015-faces-demo/src/main/java/dmit2015/faces/RectangleRequestScopedController.java
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/RectangleRequestScopedController.java
@@ -5,13 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.omnifaces.util.Messages;
 
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Named;
-
-import java.io.Serializable;
-
-@Named("currentRectangleRequestScopedController")
-@RequestScoped  // create this object for one HTTP request and destroy after the HTTP response has been sent
+//@Named("currentRectangleRequestScopedController")
+//@RequestScoped  // create this object for one HTTP request and destroy after the HTTP response has been sent
 public class RectangleRequestScopedController {
 
     @Getter @Setter
